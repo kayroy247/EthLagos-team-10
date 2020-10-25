daa3fae52f8b64dac389fea14bbffc06c4cd6063
emmaodia <odia.emma@gmail.com>
2020-10-25 17:02:22 +0100

    FIX: Changed Server Port Number.

--- Server/HashAPI/index.js
+++ Server/HashAPI/index.js
@@ -143,7 +143,7 @@ app.use((error, req, res, next) => {
 });
 
 //Server COnfiguration
-const port = 5000;  
+const port = 80;  
 app.listen(port, () => console.log(`App running on port ${port}!`))
 
 module.exports = app;
