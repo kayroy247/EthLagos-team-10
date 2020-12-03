50a68765821fcb47b48d3c5bcfcc904743655ca9
abizzy1601 <70805279+abizzy1601@users.noreply.github.com>
2020-12-03 15:36:56 +0100

    Update index.js

--- Frontend/src/App/layout/AdminLayout/NavBar/index.js
+++ Frontend/src/App/layout/AdminLayout/NavBar/index.js
@@ -21,14 +21,14 @@ class NavBar extends Component {
 
         return (
             <Aux>
-                <header style={{backgroundColor: "white"}} className={headerClass.join(' ')}>
+                <header className={headerClass.join(' ')}>
                     <div className="m-header">
                         <a className={toggleClass.join(' ')} id="mobile-collapse1" href={DEMO.BLANK_LINK} onClick={this.props.onToggleNavigation}><span/></a>
                         <a href={DEMO.BLANK_LINK} className="b-brand">
                             <div className="b-bg">
                                 <i className="feather icon-trending-up"/>
                             </div>
-                            <span className="b-title">Power Data</span>
+                            <span className="b-title">Datta Able</span>
                         </a>
                     </div>
                     <a className="mobile-menu" id="mobile-header" href={DEMO.BLANK_LINK}><i className="feather icon-more-horizontal"/></a>
